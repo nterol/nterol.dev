0ad9e84762e86882cd123440ef9999df3036c6e1
nterol <nicolas@once.app>
2023-10-18 14:40:59 +0200

    🚚  renaming post to article

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,14 @@
-
-import { NextRequest, NextResponse, userAgent } from "next/server";
+import { NextRequest, NextResponse, userAgent } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const { device } = userAgent(request);
-  const viewport = device.type === "mobile" ? "mobile" : "desktop";
+  const viewport = device.type === 'mobile' ? 'mobile' : 'desktop';
 
-  if (viewport === "mobile") {
-    
- 
-    return NextResponse.rewrite(request.url.replace("/post/", "/post/mobile/"));
+  if (viewport === 'mobile') {
+    return NextResponse.rewrite(request.url.replace('/article/', '/article/mobile/'));
   }
 }
 
 export const config = {
-  matcher: "/post/:slug*",
+  matcher: '/article/:slug*',
 };
